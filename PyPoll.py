97a82c2c57a72d754eaae06a907b9d8de135866c
dwright68 <destinwright68@outlook.com>
2021-09-18 15:06:07 -0500

    added counter for individual candidates

--- PyPoll.py
+++ PyPoll.py
@@ -7,14 +7,30 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = "analysis\election_analysis.txt"
 #create vote count variable and set to 0
 total_votes = 0
+#add candidate variable
+candidate_options = []
+#add empty dictionary 
+candidate_votes ={}
 #Open the election results and read the file
 with open(file_to_load) as election_data:
     #To do: read and analyze the election
     file_reader = csv.reader(election_data)
     #read the header row
     headers = next(file_reader)
+    
     #add up total votes
     for row in file_reader:
         total_votes +=1
+    
+    #print the candidate name from each row, then add the candidate name to the list
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+             # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+            #start counting votes    
+        candidate_votes[candidate_name] +=1
+            
+    
     #print vote count
-    print(total_votes)
+    print(candidate_votes)
